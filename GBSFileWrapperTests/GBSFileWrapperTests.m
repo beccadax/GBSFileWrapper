1ad46a81cf694621730392c73113f3d9d769d42f
Brent Royal-Gordon <brent@architechies.com>
2013-09-27 02:31:19 -0700

    Added tests for GBSFileWrapper directories

--- GBSFileWrapperTests/GBSFileWrapperTests.m
+++ GBSFileWrapperTests/GBSFileWrapperTests.m
@@ -30,6 +30,7 @@
         
     GBSFileWrapper * wrapper = [[GBSFileWrapper alloc] initWithContents:data resourceValues:@{ NSURLFileSecurityKey: [[NSFileSecurity alloc] initWithPOSIXMode:0644] }];
     
+    XCTAssertNotNil(wrapper, @"Constructed a file wrapper");
     XCTAssertEqual(wrapper.type, GBSFileWrapperTypeRegularFile, @"GBSFileWrapper with NSData contents is a regualr file");
     
     XCTAssertEqualObjects(wrapper.contents, data, @"GBSFileWrapper preserves contents correctly");
@@ -41,6 +42,28 @@
     mode_t mode;
     XCTAssertTrue([security getPOSIXMode:&mode], @"Can get POSIX mode");
     XCTAssertEqual(mode, (mode_t)0644, @"POSIX mode is correct");
+}
+
+- (void)testDirectory {
+    NSData * oneData = [@"1" dataUsingEncoding:NSUTF8StringEncoding];
+    NSData * twoData = [@"2" dataUsingEncoding:NSUTF8StringEncoding];
+    NSData * threeData = [@"3" dataUsingEncoding:NSUTF8StringEncoding];
+    
+    GBSFileWrapper * oneWrapper = [[GBSFileWrapper alloc] initWithContents:oneData resourceValues:nil];
+    GBSFileWrapper * twoWrapper = [[GBSFileWrapper alloc] initWithContents:twoData resourceValues:nil];
+    GBSFileWrapper * threeWrapper = [[GBSFileWrapper alloc] initWithContents:threeData resourceValues:nil];
+    
+    GBSFileWrapper * dir1 = [[GBSFileWrapper alloc]initWithContents:@{ @"one": oneWrapper, @"two": twoWrapper } resourceValues:nil];
+    
+    XCTAssertNotNil(dir1, @"Constructed a file wrapper");
+    XCTAssertEqual(dir1.type, GBSFileWrapperTypeDirectory, @"GBSFileWrapper constructed with a dictionary creates a directory");
+    XCTAssertEqualObjects(dir1.contents[@"one"], oneWrapper, @"dir1 contents include file one");
+    XCTAssertEqualObjects(dir1.contents[@"two"], twoWrapper, @"dir1 contents include file two");
+    
+    GBSFileWrapper * dir2 = [[GBSFileWrapper alloc] initWithContents:@{ @"three": threeWrapper, @"dir1": dir1 } resourceValues:nil];
+    XCTAssertEqualObjects(dir2.contents[@"three"], threeWrapper, @"dir2 contents include file 3");
+    XCTAssertEqualObjects(dir2.contents[@"dir1"], dir1, @"Nesting directories works");
+    XCTAssertEqualObjects([dir2.contents[@"dir1"] contents][@"one"], oneWrapper, @"Nesting directories doesn't damage the inner directory");
 }
 
 @end
